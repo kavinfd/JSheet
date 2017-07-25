182e85fe5f08fed0e0c6b1121076ce2f1058fe1a
kavin <kavinmb2@gmail.com>
2017-07-25 20:36:08 +0530

    adds a1 notation converter and tests

--- src/main/java/com/jsheet/helper/A1Notation.java
+++ src/main/java/com/jsheet/helper/A1Notation.java
@@ -14,19 +14,34 @@ import java.util.stream.Stream;
  * Created by kavin on 22/07/17.
  */
 public class A1Notation {
-    public Integer getOrdinal(Character character) {
+    private static Integer getOrdinal(Character character) {
         if ('A' <= character.charValue() && 'Z' >= character.charValue()) {
             return character.charValue() - 'A' + 1;
         }
         throw new RuntimeException(String.format("Character `%s` not in range", character));
     }
 
-    public Integer getNumber(String a1Notation) {
+    public static Integer getNumber(String a1Notation) {
         List<Character> characters = new ArrayList<>(Arrays.asList(a1Notation.chars().mapToObj(c -> (char) c).toArray(Character[]::new)));
         Collections.reverse(characters);
         return StreamUtils.zip(
-                characters.stream().map(this::getOrdinal),
-                IntStream.iterate(0, x -> x+1).map(x -> (int)Math.pow(26, x)).mapToObj(Integer::new),
+                characters.stream().map(A1Notation::getOrdinal),
+                IntStream.iterate(0, x -> x + 1).map(x -> (int) Math.pow(26, x)).mapToObj(Integer::new),
                 (x, y) -> x * y).mapToInt(Integer::intValue).sum();
+    }
+
+    public static String getAlphaNotation(Integer value) {
+        if (value < 1) {
+            throw new RuntimeException("Alpha notation value must be greater than or equal to 1");
+        }
+        String memo = "ZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String result = "";
+        while (value > 0) {
+            Integer index = value % 26;
+            result = memo.charAt(index)+result;
+            value--;
+            value /= 26;
+        }
+        return result;
     }
 }
--- src/test/com/jsheet/helper/A1NotationTest.java
+++ src/test/com/jsheet/helper/A1NotationTest.java
@@ -1,5 +1,6 @@
 package com.jsheet.helper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,12 +10,27 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  * Created by kavin on 22/07/17.
  */
 public class A1NotationTest {
+
     @Test
     public void testGetNumber() throws Exception {
-        A1Notation a1Notation = new A1Notation();
-        assertThat(a1Notation.getNumber("A")).isEqualTo(1);
-        assertThat(a1Notation.getNumber("Z")).isEqualTo(26);
-        assertThat(a1Notation.getNumber("AA")).isEqualTo(27);
-        assertThat(a1Notation.getNumber("AZ")).isEqualTo(52);
+        assertThat(A1Notation.getNumber("A")).isEqualTo(1);
+        assertThat(A1Notation.getNumber("Z")).isEqualTo(26);
+        assertThat(A1Notation.getNumber("AA")).isEqualTo(27);
+        assertThat(A1Notation.getNumber("AZ")).isEqualTo(52);
+    }
+
+    @Test
+    public void testGetAlphaNotation() throws Exception {
+        assertThat(A1Notation.getAlphaNotation(10)).isEqualTo("J");
+        assertThat(A1Notation.getAlphaNotation(12)).isEqualTo("L");
+        assertThat(A1Notation.getAlphaNotation(52)).isEqualTo("AZ");
+    }
+
+    @Test
+    public void testGetAlphaNotationAndGetNumber() throws Exception {
+        assertThat(A1Notation.getNumber(A1Notation.getAlphaNotation(1123))).isEqualTo(1123);
+        assertThat(A1Notation.getNumber(A1Notation.getAlphaNotation(2144))).isEqualTo(2144);
+        assertThat(A1Notation.getNumber(A1Notation.getAlphaNotation(10324))).isEqualTo(10324);
+        assertThat(A1Notation.getNumber(A1Notation.getAlphaNotation(12340))).isEqualTo(12340);
     }
 }
